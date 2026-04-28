7dcafbd252047421b33d43a3705f0babf4b46e4a
Luna Amadeus <leo.amadeus.scott@gmail.com>
2026-04-28 15:30:05 +1000

    repeatString complete

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = (string, num) => {
+    if (num < 0) {
+        return "ERROR";
+    }
+    let finalString = "";
+    for (let a = 1; a <= num; a ++) {
+        finalString += string;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
