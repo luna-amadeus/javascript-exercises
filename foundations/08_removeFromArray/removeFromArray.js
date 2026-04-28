46193ac245a09ea6c56406ea5066a8ab051ab59e
Luna Amadeus <leo.amadeus.scott@gmail.com>
2026-04-28 15:43:03 +1000

    fix function

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = (inputArr, ...toRemove) => {
+    return inputArr.filter((str) => {
+        return !toRemove.includes(str);
+    })
 };
 
 // Do not edit below this line
