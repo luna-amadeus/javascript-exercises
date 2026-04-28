a97fa2aee021a462d65ab033e2590b7032985c46
Luna Amadeus <leo.amadeus.scott@gmail.com>
2026-04-28 22:12:21 +1000

    fix sumAll();

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = (firstNum, secondNum) => {
+    //check validity of numbers
+    if (typeof firstNum !== "number" || typeof secondNum !== "number" || Number.isInteger(firstNum) === false || Number.isInteger(secondNum) === false || firstNum < 0 || secondNum < 0) {
+        return "ERROR";
+    }
+    //put lowest number first
+    let smallNum;
+    let largeNum;
+    if (firstNum > secondNum) {
+        smallNum = secondNum;
+        largeNum = firstNum;
+    } else {
+        smallNum = firstNum;
+        largeNum = secondNum;
+    }
+    const sumArr = [];
+    for (let a = smallNum; a <= largeNum; a ++) {
+        sumArr.push(a);
+    }
+    return sumArr.reduce((accumulator, currentVal) => {
+        return accumulator + currentVal;
+    }, 0);
 };
 
 // Do not edit below this line
