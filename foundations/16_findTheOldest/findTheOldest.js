7f0d8ddef98e5a0ca639295c0066c243eb208648
Luna Amadeus <leo.amadeus.scott@gmail.com>
2026-04-29 23:37:50 +1000

    findTheOldest()

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arrOfObjects) {
+    return arrOfObjects.reduce((oldest, next) => {
+        if (next.yearOfDeath === undefined) {next.yearOfDeath = 2026};
+        if (oldest.yearOfDeath - oldest.yearOfBirth > next.yearOfDeath - next.yearOfBirth) {
+            return oldest;
+        } else {
+            return next;
+        }
+    }, arrOfObjects[0])
 };
 
 // Do not edit below this line
