b9e4b5be813acd822fec94d8c64fb29892363a3c
Luna Amadeus <leo.amadeus.scott@gmail.com>
2026-04-29 23:21:44 +1000

    add palindrome function

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const filtStr = str.replace(/[^\w\s\']/g, "").replace(/\s+/g, "").toLowerCase();
+    console.log(filtStr);
+    return filtStr === filtStr.split("").reverse().join("");
 };
 
 // Do not edit below this line
