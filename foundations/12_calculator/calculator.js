7f51abe5d5c27af5388ec64139affc15733ab24e
Luna Amadeus <leo.amadeus.scott@gmail.com>
2026-04-29 23:09:00 +1000

    add calculator functions

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((accumulator, num) => accumulator + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((accumulator, num) => accumulator * num, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	const factArr = [];
+  for (let a = num; a >= 1; a --) {
+    factArr.push(a);
+  }
+  return factArr.reduce((accumulator, number) => accumulator * number, 1);
 };
 
 // Do not edit below this line
