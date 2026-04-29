5f4961133de821584ba6eb842020564742db27a0
Luna Amadeus <leo.amadeus.scott@gmail.com>
2026-04-29 23:27:42 +1000

    Copy fibonacci function

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0) return "OOPS";
+    let [a, b] = [0, 1];
+    for (let pos = 0; pos < n; pos ++) {
+        [a, b] = [b, a + b];
+    }
+    return a;
 };
 
 // Do not edit below this line
